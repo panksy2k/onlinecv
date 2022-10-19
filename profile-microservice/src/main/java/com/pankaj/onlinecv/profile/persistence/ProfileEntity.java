021d2d3a34f551559166353ca2daf25e2fd0dc44
Pankaj Pardasani <pankaj.d.p@gmail.com>
2022-10-19 23:22:14 +0100

    First sccafolding of the proj

---
+++ profile-microservice/src/main/java/com/pankaj/onlinecv/profile/persistence/ProfileEntity.java
@@ -0,0 +1,7 @@
+package com.pankaj.onlinecv.profile.persistence;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "profile")
+public class ProfileEntity {
+}
